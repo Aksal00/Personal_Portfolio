188f063666c61b14a4152e287fecfe13886e2549
Akila Srikantha <110421821+Aksal00@users.noreply.github.com>
2024-05-16 14:43:42 +0530

    abc

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -55,15 +55,7 @@ const projectsData = [
     gitUrl: "/",
     previewUrl: "/",
   },
-  {
-    id: 6,
-    title: "Full-stack Roadmap",
-    description: "Project 5 description",
-    image: "/images/projects/6.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
+  
 ];
 
 const ProjectsSection = () => {
