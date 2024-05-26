6e8b8b5c2daf2cb03f70a8a213ed0fae9363348c
Akila Srikantha <110421821+Aksal00@users.noreply.github.com>
2024-05-26 14:35:12 +0530

    icon updated

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,17 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import icon from "./icon.png"
+import apple_icon from "./apple-icon.png"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Akila Srikantha",
   description: "Personal Portfolio",
+  icons: {
+    icon: icon,
+    apple: apple_icon,
+  },
 };
 
 export default function RootLayout({ children }) {
