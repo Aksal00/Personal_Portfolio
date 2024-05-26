f3e0d5d96e5046634893ee95ec8cf41d2073113e
Akila Srikantha <110421821+Aksal00@users.noreply.github.com>
2024-05-26 17:19:19 +0530

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container sm:px-4 py-12 md:p-12 flex justify-between">
+    <footer className="footer flex flex-col border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container sm:px-4 pt-5 pb-2 flex justify-center">
         <p>
         <Link href={"/"} className="flex items-center text-xl md:text-3xl text-white font-light">
             <Image 
@@ -28,8 +28,10 @@ const Footer = () => {
             <span>rikantha</span>
         </Link>
         </p>
-        <p className="text-slate-600">All rights reserved.</p>
-      </div>
+        </div>
+        <div className="flex justify-center pb-2"><p className="text-slate-500">All rights reserved.</p></div>
+        
+      
     </footer>
   );
 };
