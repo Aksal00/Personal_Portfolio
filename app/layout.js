7c623b1fe089b3be56adb94ad78664e0448a249d
Akila Srikantha <110421821+Aksal00@users.noreply.github.com>
2024-06-02 16:20:40 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Akila Srikantha",
   description: "Personal Portfolio",
-  
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '192x192',
+      url: '/icon.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/apple-icon.png',
+    },
+  ]
 };
 
 export default function RootLayout({ children }) {
